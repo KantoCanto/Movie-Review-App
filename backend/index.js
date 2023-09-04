cd65c6f6603379f9a86bffe84c39619872c09680
KantoCanto <bruno.canto.neves@gmail.com>
2023-09-04 13:21:22 +0100

    created movies dao (data access object)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import MoviesDAO from "./dao/moviesDAO.js"
 
 async function main(){
 
@@ -13,6 +14,7 @@ async function main(){
     try{
         //Connect to the MOngoDb cluster
         await client.connect()
+        await MoviesDAO.injectDB(client);
 
         app.listen(port, () =>{
             console.log("server is runnning on port: " + port)
