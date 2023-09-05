1174568ee159fce4ac1dfd114cdb537694be72ac
KantoCanto <bruno.canto.neves@gmail.com>
2023-09-05 11:16:14 +0100

    all routes are connected. added apiGetMovieById and apiGetRatings backend is completed

--- backend/api/movies.controller.js
+++ backend/api/movies.controller.js
@@ -2,7 +2,7 @@ import MoviesDAO from "../dao/moviesDAO.js";
 
 export default class MoviesController{
 
-    static async apiGetMovies(req,res,next){
+    static async apiGetMovies(req, res, next){
         const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20;
         const page = req.query.page ? parseInt(req.query.page) : 0;
 
@@ -24,5 +24,30 @@ export default class MoviesController{
         }
 
         res.json(response);
+    }
+
+    static async apiGetMovieById(req, res, next){
+        try{
+            let id = req.params.id || {};
+            let movie = await MoviesDAO.getMovieById(id);
+            if(!movie){
+                res.status(404).json({error: "not found"});
+                return;
+            }
+            res.json(movie);
+        } catch(e){
+            console.log(`api, ${e}`);
+            res.status(500).json({error: e});
+        }
+    }
+
+    static async apiGetRatings(req, res, next){
+        try{
+            let propertyTypes = await MoviesDAO.getRatings();
+            res.json(propertyTypes);
+        } catch(e){
+            console.log(`api, ${e}`);
+            res.status(500).json({error: e})
+        }
     }
 }
