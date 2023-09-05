38565efca69428f686ffaa3496f742ae69d27132
KantoCanto <bruno.canto.neves@gmail.com>
2023-09-05 14:00:12 +0100

    added frontend through react, react-router-dom and react-bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Switch, Route, Link} from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World? :)</h1>
+    </div>
+  );
+}
+
+export default App;
