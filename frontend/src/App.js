406461e3059a586326b73309067503b3fc39d0c5
KantoCanto <bruno.canto.neves@gmail.com>
2023-09-05 15:05:23 +0100

    created navigation header bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import {React, useState} from "react";
 import {Switch, Route, Link} from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+//import react-bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+//import components
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  async function login(user = null){
+    setUser(user);
+  }
+
+  async function logout(){
+    setUser(null);
+  }
+
   return (
     <div className="App">
-      <h1>Hello World? :)</h1>
+      <Navbar bg="dark" ecpand="lg">
+        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle></Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                  <a onClick={logout}>Logout User</a>
+                ) : (
+                  <Link to={"/login"}>Login</Link>
+                )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
